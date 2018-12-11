13bf862610ee2f1e7c9baf5afda208d6e539f764
Ole Martin Gjersvik <olgj@equinor.com>
2018-12-11 18:37:02 +0100

    Turned TestActions into an actor.

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,27 @@ mod models;
 mod test_actions;
 mod views;
 
+use actix_web::error::ErrorInternalServerError;
+use actix_web::AsyncResponder;
+use actix_web::Error;
+use futures::future::Future;
+use crate::test_actions::{GetActions, TestActions} ;
 use actix_web::{server, App, HttpRequest, HttpResponse};
+use actix::Arbiter;
 
-fn index(_req: &HttpRequest) -> HttpResponse {
-    let test = test_actions::test_actions();
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(views::view_list(&test))
+fn index(_req: &HttpRequest) -> Box<Future<Item=HttpResponse, Error=Error>> {
+    let test = Arbiter::registry().get::<TestActions>();
+
+    let res = test.send(GetActions{});
+
+    res.map(|r| {r.unwrap_or_default()})
+        .map(|test| {
+            HttpResponse::Ok()
+            .content_type("text/html")
+            .body(views::view_list(&test))
+        })
+        .map_err(|_| {ErrorInternalServerError("Ups")})
+        .responder()
 }
 
 /// Start a server and call the `Handler` we've defined above for each `Request` we receive.
--- src/test_actions.rs
+++ src/test_actions.rs
@@ -4,12 +4,35 @@ use crate::models::{
     Action
 };
 
+pub struct GetActions;
 
-pub fn test_actions() -> Vec<Action> {
-    vec![
-        test_action("Play Games", ActionType::Entertainment),
-        test_action("Do dishes", ActionType::Task( Priority::Important)),
-    ]
+impl actix::Message for GetActions {
+    type Result = Result<Vec<Action>,()>;
+}
+
+#[derive(Default)]
+pub struct TestActions;
+
+impl actix::Actor for TestActions {
+    type Context = actix::Context<Self>;
+}
+
+impl actix::Supervised for TestActions {}
+
+impl actix::ArbiterService for TestActions {
+   fn service_started(&mut self, ctx: &mut actix::Context<Self>) {
+   }
+}
+
+impl actix::Handler<GetActions> for TestActions {
+   type Result = Result<Vec<Action>,()>;
+
+    fn handle(&mut self, _: GetActions, _: &mut actix::Context<Self>) -> Self::Result{
+        Ok(vec![
+            test_action("Play Games", ActionType::Entertainment),
+            test_action("Do dishes", ActionType::Task( Priority::Important)),
+        ])
+    }
 }
 
 fn test_action(name: &str, action_type: ActionType) -> Action{
