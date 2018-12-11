53ba671112e537ee67d29a850fb6954ff74a518b
Ole Martin Gjersvik <olgj@equinor.com>
2018-12-11 16:49:35 +0100

    Chanced gotham with actix.

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,13 @@ mod models;
 mod test_actions;
 mod views;
 
-use gotham::state::State;
+use actix_web::{server, App, HttpRequest, HttpResponse};
 
-/// Create a `Handler` which calls the Tera static reference, renders
-/// a template with a given Context, and returns the result as a String
-/// to be used as Response Body
-pub fn say_hello(state: State) -> (State, (mime::Mime, String)) {
+fn index(_req: &HttpRequest) -> HttpResponse {
     let test = test_actions::test_actions();
-    let rendered = views::view_list(&test);
-
-    (state, (mime::TEXT_HTML, rendered))
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(views::view_list(&test))
 }
 
 /// Start a server and call the `Handler` we've defined above for each `Request` we receive.
@@ -22,5 +19,8 @@ pub fn start() {
     println!("{:?}", std::env::current_exe());
     let addr = "127.0.0.1:7878";
     println!("Listening for requests at http://{}", addr);
-    gotham::start(addr, || Ok(say_hello))
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind(addr)
+        .unwrap()
+        .run();
 }
