9d3ecaa225717f68b0ac5cbd2e2cedee8ec737d1
Ole Martin Gjersvik <olgj@equinor.com>
2018-12-13 14:42:21 +0100

    Show from both sources.

--- src/lib.rs
+++ src/lib.rs
@@ -10,25 +10,32 @@ use actix_web::AsyncResponder;
 use actix_web::Error;
 use futures::future::Future;
 use actix_web::{server, App, HttpRequest, HttpResponse};
-use actix::Arbiter;
+use actix::{Arbiter, System};
 
 use crate::{
     models::GetActions,
     test_actions::TestActions,
+    housework::Chores,
 };
 
 fn index(_req: &HttpRequest) -> Box<Future<Item=HttpResponse, Error=Error>> {
     let test = Arbiter::registry().get::<TestActions>();
+    let chore = System::current().registry().get::<Chores>();
 
-    let res = test.send(GetActions{});
+    let res = test.send(GetActions{}).join(chore.send(GetActions{}));
 
-    res.map(|actions| {
-            HttpResponse::Ok()
-            .content_type("text/html")
-            .body(views::view(&actions.actions))
-        })
-        .map_err(|_| {ErrorInternalServerError("Ups")})
-        .responder()
+    res.map(|(a,b)|{
+        let mut a = a.actions;
+        let mut b = b.actions;
+        a.append(&mut b);
+        a
+    }).map(|actions| {
+        HttpResponse::Ok()
+        .content_type("text/html")
+        .body(views::view(&actions))
+    })
+    .map_err(|_| {ErrorInternalServerError("Ups")})
+    .responder()
 }
 
 /// Start a server and call the `Handler` we've defined above for each `Request` we receive.
