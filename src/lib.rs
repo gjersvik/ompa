7701cd42cabb568256caa26006de7ec00aace7bc
Ole Martin Gjersvik <olem@olem.org>
2018-12-16 10:09:53 +0100

    Added heroku port.

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ mod test_actions;
 
 use actix::{Actor, System};
 use actix_web::server;
+use std::env;
 
 use crate::{
     test_actions::TestActions,
@@ -18,13 +19,14 @@ use crate::{
 /// Start a server and call the `Handler` we've defined above for each `Request` we receive.
 pub fn start() {
     let sys = System::new("ompa");
+    let port = env::var("PORT").unwrap_or_else(|_| "7878".to_string());
 
     let _test = TestActions::start_default();
     let _chore = Chores::start_default();
     let _log = Logger::addr();
 
     let server = server::new(OleMartin::app);
-    let addr = "127.0.0.1:7878";
+    let addr = "0.0.0.0:".to_string() + &port;
     println!("Listening for requests at http://{}", addr);
     let server = server.bind(addr).unwrap();
     server.start();
