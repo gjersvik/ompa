491b35874ff62685ba7438a33f1ddeb070328b58
Ole Martin Gjersvik <olem@olem.org>
2018-12-16 13:36:11 +0100

    Added simple auth

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,17 @@ mod ole_martin;
 mod test_actions;
 
 use actix::{Actor, System};
-use actix_web::server;
-use std::env;
+use actix_web::{
+    server,
+    Result,
+    HttpRequest,
+    FromRequest,
+    middleware::{Middleware, Started},
+};
+use actix_web_httpauth::extractors::{
+    AuthenticationError,
+    basic::{BasicAuth, Config as AuthConfig},
+};
 
 use crate::{
     test_actions::TestActions,
@@ -16,17 +25,49 @@ use crate::{
     logger::Logger,
 };
 
-/// Start a server and call the `Handler` we've defined above for each `Request` we receive.
-pub fn start() {
+#[derive(Clone)]
+struct Auth{
+    password: String,
+}
+
+impl Auth {
+    fn new(password: String) -> Auth{
+        Auth {password}
+    }
+}
+
+impl<S> Middleware<S> for Auth {
+    fn start(&self, req: &HttpRequest<S>) -> Result<Started> {
+        let mut config = AuthConfig::default();
+        config.realm("Ompa");
+        let auth = BasicAuth::from_request(&req, &config)?;
+
+        if auth.username() == "olem" && auth.password() == Some(&self.password) {
+            Ok(Started::Done)
+        } else {
+            Err(AuthenticationError::from(config).into())
+        }
+    }
+}
+
+pub struct Config{
+    pub bind_port: String,
+    pub web_password: String,
+}
+
+pub fn start(config: Config) {
     let sys = System::new("ompa");
-    let port = env::var("PORT").unwrap_or_else(|_| "7878".to_string());
 
     let _test = TestActions::start_default();
     let _chore = Chores::start_default();
     let _log = Logger::addr();
 
-    let server = server::new(OleMartin::app);
-    let addr = "0.0.0.0:".to_string() + &port;
+    let auth = Auth::new(config.web_password);
+
+    let server = server::new(move || {
+        OleMartin::app().middleware(auth.clone())
+    });
+    let addr = "0.0.0.0:".to_string() + &config.bind_port;
     println!("Listening for requests at http://{}", addr);
     let server = server.bind(addr).unwrap();
     server.start();
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use ompa::Config;
+use std::env;
+
 fn main() {
-    ompa::start();
+    dotenv::dotenv().ok();
+
+    let config = Config {
+        bind_port: env::var("PORT").expect("PORT not found"),
+        web_password: env::var("OMPA_PASSWORD").expect("OMPA_PASSWORD not found")
+    };
+
+    ompa::start(config);
 }
