38f84acd602cd8c666adb0e8bb7d8233eb5c1e86
Ole Martin Gjersvik <olem@olem.org>
2018-12-27 09:35:35 +0100

    Todoist to actions.

--- src/todoist.rs
+++ src/todoist.rs
@@ -5,17 +5,80 @@ use self::{
     service::Service,
     messages::Sync,
 };
-use actix::{SyncArbiter,Arbiter};
+use crate::ole_martin::{Action, ActionType, CompletedSub, Priority, UpdateActions,Completed};
+use actix::{
+    fut::{wrap_future, ActorFuture},
+    Actor, Addr, AsyncContext, Context, Recipient, SyncArbiter,Handler,
+};
 use futures::future::Future;
+use todoist::resource::Item;
+use std::{
+    collections::HashMap,
+    time::Duration,
+};
 
-pub fn todoist(token: String){
-    let service = SyncArbiter::start(1, move|| Service::new(token.clone()));
+pub struct Todoist{
+    items: HashMap<usize, Item>,
+    sync_token: Option<String>,
+    service: Addr<Service>,
+    update_action: Recipient<UpdateActions>,
+}
 
-    let run = service.send(Sync(None)).map(|res| {
-        for item in res.items {
-            println!("{}", item.content.unwrap_or_default())
-        };
-    }).map_err(|_|{});
+impl Todoist {
+    pub fn new(token: String, update_action: Recipient<UpdateActions>) -> Todoist {
+        Todoist {
+            items: HashMap::new(),
+            sync_token: None,
+            service: SyncArbiter::start(1, move|| Service::new(token.clone())),
+            update_action,
+        }
+    }
 
-    Arbiter::spawn(run);
+    fn update(&mut self, ctx: &mut Context<Self>){
+        let load = self.service.send(Sync(None)).map_err(|_| {});
+
+        let update_self = wrap_future::<_, Self>(load).map(|res, actor, _| {
+            actor.sync_token = res.sync_token;
+            if res.items.is_empty() {
+                return;
+            }
+            for item in res.items {
+                actor.items.insert(item.id, item);
+            }
+            actor.update_action.do_send(UpdateActions {
+                name: name(),
+                actions: items_to_actions(&actor.items),
+            }).unwrap_or_default();
+        });
+
+        ctx.spawn(update_self);
+    }
+}
+
+impl Actor for Todoist {
+    type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Context<Self>) {
+        self.update(ctx);
+
+        ctx.run_interval(Duration::from_secs(5*60), Self::update);
+    }
+}
+
+fn items_to_actions(items: &HashMap<usize, Item>) -> Vec<Action> {
+    items.values().filter_map(|item| {
+        if item.checked == 0 {
+            Some(Action {
+                index: item.id,
+                name: item.content.clone().unwrap_or_default(),
+                action_type: ActionType::Task(Priority::JustForFun),
+            })
+        }else{
+            None
+        }
+    }).collect()
+}
+
+fn name() -> String{
+    "todoist".to_string()
 }
