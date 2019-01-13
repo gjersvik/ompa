dcf7fe310e7a211462510e27f938d5bb70b95ba9
Ole Martin Gjersvik <olem@olem.org>
2019-01-13 12:53:13 +0100

    Written tests for get_action_priority

--- src/personal/todoist.rs
+++ src/personal/todoist.rs
@@ -143,42 +143,97 @@ mod tests {
     use super::*;
 
     use chrono::offset::TimeZone;
+    use lazy_static::lazy_static;
 
-    fn test_priority(pri: TodoistPriority) -> Option<Priority>{
+    lazy_static! {
+        static ref NOW:DateTime<Utc> = Utc.ymd(2010,1,1).and_hms(0,0,0);
+    }
+
+    fn test_priority(pri: TodoistPriority) -> Priority{
+        test_priority_base(pri, None)
+    }
+
+    fn test_priority_date(pri: TodoistPriority) -> Priority{
+        test_priority_base(pri, Some(*NOW + Duration::days(1) + Duration::seconds(1)))
+    }
+
+    fn test_priority_last_day(pri: TodoistPriority) -> Priority{
+        test_priority_base(pri, Some(*NOW + Duration::days(1)))
+    }
+
+    fn test_priority_base(pri: TodoistPriority, due: Option<DateTime<Utc>>) -> Priority {
         let item = Item{
             id:0,
             content: "test".to_string(),
-            due_date_utc: None,
+            due_date_utc: due,
             priority: pri,
         };
 
-        let now = Utc.ymd(2010,1,1).and_hms(0,0,0);
-
-        if let ActionType::Task(pri) = item.to_action(&now).action_type {
+        if let ActionType::Task(pri) = item.to_action(&NOW).action_type {
             Some(pri)
         } else {
             None
-        }
+        }.unwrap()
     }
 
     #[test]
     fn item_get_action_priority_p1() {
-        assert_eq!(test_priority(TodoistPriority::P1).unwrap(), Priority::Important);
+        assert_eq!(test_priority(TodoistPriority::P1), Priority::Important);
     }
 
     #[test]
     fn item_get_action_priority_p2() {
-        assert_eq!(test_priority(TodoistPriority::P2).unwrap(), Priority::Useful);
+        assert_eq!(test_priority(TodoistPriority::P2), Priority::Useful);
     }
 
     #[test]
     fn item_get_action_priority_p3() {
-        assert_eq!(test_priority(TodoistPriority::P3).unwrap(), Priority::NiceToHave);
+        assert_eq!(test_priority(TodoistPriority::P3), Priority::NiceToHave);
     }
 
     #[test]
     fn item_get_action_priority_p4() {
-        assert_eq!(test_priority(TodoistPriority::P4).unwrap(), Priority::JustForFun);
+        assert_eq!(test_priority(TodoistPriority::P4), Priority::JustForFun);
+    }
+
+    #[test]
+    fn item_get_action_priority_p1_date() {
+        assert_eq!(test_priority_date(TodoistPriority::P1), Priority::VeryImportant);
+    }
+
+    #[test]
+    fn item_get_action_priority_p2_date() {
+        assert_eq!(test_priority_date(TodoistPriority::P2), Priority::Important);
+    }
+
+    #[test]
+    fn item_get_action_priority_p3_date() {
+        assert_eq!(test_priority_date(TodoistPriority::P3), Priority::Useful);
+    }
+
+    #[test]
+    fn item_get_action_priority_p4_date() {
+        assert_eq!(test_priority_date(TodoistPriority::P4), Priority::NiceToHave);
+    }
+
+    #[test]
+    fn item_get_action_priority_p1_last_day() {
+        assert_eq!(test_priority_last_day(TodoistPriority::P1), Priority::Critical);
+    }
+
+    #[test]
+    fn item_get_action_priority_p2_last_day() {
+        assert_eq!(test_priority_last_day(TodoistPriority::P2), Priority::VeryImportant);
+    }
+
+    #[test]
+    fn item_get_action_priority_p3_last_day() {
+        assert_eq!(test_priority_last_day(TodoistPriority::P3), Priority::Important);
+    }
+
+    #[test]
+    fn item_get_action_priority_p4_last_day() {
+        assert_eq!(test_priority_last_day(TodoistPriority::P4), Priority::Useful);
     }
 
 }
